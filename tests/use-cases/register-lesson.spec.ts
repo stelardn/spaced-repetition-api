754f60088528b3d9096bedb5adc1fe0a3372d588
Stéfany Larissa <stelar.dev@gmail.com>
2024-12-29 14:01:34 -0300

    tests (RegisterLessonUseCase): adds revisions interval check

--- tests/use-cases/register-lesson.spec.ts
+++ tests/use-cases/register-lesson.spec.ts
@@ -1,4 +1,4 @@
-import { describe, beforeEach, it, expect } from 'vitest'
+import { describe, beforeEach, it, expect, afterEach, vi } from 'vitest'
 import { RegisterLessonUseCase } from '../../src/domain/calendar/application/use-cases/register-lesson';
 import { LessonsRepository } from '../../src/domain/calendar/application/repositories/LessonsRepository'
 import { LessonsRepositoryInMemory } from '../repositories/LessonsRepositoryInMemory';
@@ -14,10 +14,18 @@ describe('RegisterLessonUseCase Unit Tests', async () => {
   beforeEach(() => {
     lessonsRepository = new LessonsRepositoryInMemory()
     revisionsRepository = new RevisionsRepositoryInMemory()
+
+    vi.useFakeTimers()
+    vi.setSystemTime(new Date(2024, 11, 31));
+
     sut = new RegisterLessonUseCase(
       lessonsRepository, 
       revisionsRepository
     )
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('must be possible to register a study', async () => {
@@ -28,5 +36,16 @@ describe('RegisterLessonUseCase Unit Tests', async () => {
     expect(result).toBeInstanceOf(Lesson)
     expect(lessonsRepository.items).toHaveLength(1)
     expect(revisionsRepository.items).toHaveLength(3)
+  })
+
+  it('must create revisions in the defined spaced repetition intervals', async () => {
+    const result = await sut.execute({
+      subject: faker.word.words(2),
+      theme: faker.word.words(3),
+    })
+
+    expect(revisionsRepository.items[0].dueDate).toEqual(new Date(2025, 0, 1))
+    expect(revisionsRepository.items[1].dueDate).toEqual(new Date(2025, 0, 7))
+    expect(revisionsRepository.items[2].dueDate).toEqual(new Date(2025, 0, 30))
   })
 })
