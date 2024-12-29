e7ec340443191cc6ab98f45b835b398a9e51f702
Stéfany Larissa <stelar.dev@gmail.com>
2024-12-29 15:37:02 -0300

    chore (vistest config): sets import to vite instead of vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
-import {defineConfig} from 'vitest/config'
+import { defineConfig } from 'vite'
 import tsconfigpaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsconfigpaths()],
   test: {
     globals: true,
     environment: 'node',
     include: ['tests/**/*.{test,spec}.ts']
   },
-  plugins: [tsconfigpaths()]
 })
