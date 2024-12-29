5533b37e37d8fb28051c5694e921ccbf0cce8b3b
Stéfany Larissa <stelar.dev@gmail.com>
2024-12-29 14:53:40 -0300

    chore: moves app error into core

--- src/domain/entities/errors/invalid-date-error.ts
+++ src/domain/entities/errors/invalid-date-error.ts
@@ -1,4 +1,4 @@
-import { AppError } from "./app-error";
+import { AppError } from "../../../core/errors/app-error";
 
 export class InvalidDateError extends AppError {
   constructor(inputDate: string) {
